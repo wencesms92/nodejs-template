853ffc3f751f23eec9dae6384d4e7b2da8a3f487
wencesms92 <wencesms92@gmail.com>
2025-06-16 14:51:23 +0200

    test(env): add env var for testing purposes

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
+const dashApiKey = process.env.DASH_API_KEY || 'default';
 
 // Define a route for the root URL ("/")
 app.get('/', (req, res) => {
-  res.send('Hello, World! This is a test for env var DASH_API_KEY: ' + process.env.DASH_API_KEY);
+  res.send('Hello, World! This is a test for env var DASH_API_KEY: ' + dashApiKey);
 });
 
 // Start the server
